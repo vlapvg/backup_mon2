7121e3c1dac714aec4d50c748e2ce682223b6126
Vladimir Paltsev <vlap-d@yandex.ru>
2025-12-16 21:04:59 +0500

    Update cust_2.sh

--- cust_2.sh
+++ cust_2.sh
@@ -1,7 +1,8 @@
-#!/Bin/bash
-cp ports.conf /etc/apache2; systemctl start apache2; systemctl enable apache2; 
+#!/bin/bash
+cp ports.conf /etc/apache2; cp 000-default.conf /etc/apache2/sites-enabled;
+\cp index.html /var/www/html; systemctl start apache2; 
 \cp filebeat.yml /etc/filebeat; systemctl restart filebeat; 
-\cp mysqld.cnf /etc/mysql/mysql.d; service mysql restart
+\cp mysqld.cnf /etc/mysql/mysql.conf.d; service mysql restart; 
 \cp prometheus.yml /etc/prometheus; systemctl restart prometheus; 
 \systemctl restart prometheus-node-exporter; 
-\systemctl restart grafana-server;
+\systemctl restart grafana-server
