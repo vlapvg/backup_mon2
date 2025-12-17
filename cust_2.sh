99ab26a42caa761d1910941913ed4f40f97cce31
Vladimir Paltsev <vlap-d@yandex.ru>
2025-12-17 10:34:40 +0500

    Update cust_2.sh

--- cust_2.sh
+++ cust_2.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cp ports.conf /etc/apache2; cp 000-default.conf /etc/apache2/sites-enabled;
+systemctl stop apache2; systemctl start nginx; 
+\cp ports.conf /etc/apache2; cp 000-default.conf /etc/apache2/sites-enabled;
 \cp index.html /var/www/html; systemctl start apache2; 
 \cp filebeat.yml /etc/filebeat; systemctl restart filebeat; 
 \cp mysqld.cnf /etc/mysql/mysql.conf.d; service mysql restart; 
